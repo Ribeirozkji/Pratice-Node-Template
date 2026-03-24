9744b254f2bf8495baefd795f4057e38d04937c7
Imóvel <phassumpcao346@gmail.com>
2026-03-24 11:03:20 -0300

    primeiro commit

--- AULA21/node_sql/index.js
+++ AULA21/node_sql/index.js
@@ -1,68 +1,68 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
+const path = require("path");
 
 const app = express();
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.json())
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
-// Configuração do mecanismo de visualização handlebars
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static('public'));
-
-// Rota para renderizar o arquivo home.handlebars
-app.get('/', (req, res) => {
-  res.render('layouts/home');
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "teste34",
 });
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.post('/teste/insertteste', (req, res) => {
-    const nome = req.body.nome
-    const email = req.body.email
-    const sql = ` INSERT INTO teste (nome, email) VALUES  ('${nome}', '${email}') `
-    conn.query(sql, function(err){
-        if (err) {
-            console.log(err);
-          }
-          res.redirect('/teste')
-    })
-})
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const senha = req.body.senha;
 
-app.get('/teste', (req, res)=> {
-  const sql= "SELECT * FROM teste"
+  const sql = 'INSERT INTO teste (nome, email, senha) VALUES (?, ?, ?)';
 
-  conn.query(sql, function  (err, data){
+  conn.query(sql, [nome, email, senha], function(err) {
     if (err) {
       console.log(err);
-     return
+      return res.status(500).send('Erro ao inserir dados');
     }
-    const teste =data;
-    console.log(teste);
 
-    res.render('layouts/teste', {teste} );
+    res.redirect('/teste');
   });
 });
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'teste34',
+
+app.get("/teste", (req, res) => {
+  const sql = "SELECT * FROM teste";
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Erro ao buscar dados");
+    }
+
+    res.render("teste", { teste: data });
+  });
 });
 
-conn.connect(function(err) {
+conn.connect(function (err) {
   if (err) {
     console.log(err);
+    return;
   }
-  console.log('Conectado ao MySQL');
-  app.listen(3000);
+
+  console.log("Conectado ao MySQL");
+  app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+  });
 });
